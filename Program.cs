6856d15eeca08cd33b596e50ab27a7a47a3747a1
Александр Пелипенко <vk-tester@yandex.ru>
2021-01-14 21:11:15 +0600

    Init commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuadraticEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    int a = rand.Next(-50, 50);
+                    int b = rand.Next(-50, 50);
+                    int c = rand.Next(-50, 50);
+                    Quadric quadric = new Quadric(a, b, c);
+                    Console.WriteLine($"Вычисление {i}");
+                    quadric.FindSquare();
+                    quadric.FindExtremum();
+                    quadric.CalculateIntervals();
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("Ошибка");
+                }
+                
+            }
+
+        }
+    }
+}
---
+++ Quadric.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuadraticEquation
+{
+    class Quadric
+    {
+        private double x1;
+        private double x2;
+        private readonly int a;
+        private readonly int b;
+        private readonly int c;
+        private double x0;
+        const string up = "возрастает";
+        const string down = "убывает";
+        public Quadric(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public void FindSquare()
+        {
+            double D = Math.Pow(b, 2) - 4 * a * c;
+            if (D > 0 || D == 0)
+            {
+                x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                Console.WriteLine($"x1= {x1}\nx2= {x2}");
+            }
+
+            else
+            {
+                Console.WriteLine("Действительных корней нет");
+            }
+        }
+
+        public void FindExtremum()
+        {
+            x0 = -b / (2d * a);
+            double y0 = a * Math.Pow(x0, 2) + b * x0 + c;
+            Console.WriteLine($"S({x0};{y0})");
+        }
+
+        public void CalculateIntervals()
+        {
+            bool? isGrowing = null; //а=0 - признак линейной функции
+            if (a > 0)
+            {
+                isGrowing = true; //возрастает
+            }
+            else if (a < 0)
+            {
+                isGrowing = false;
+            }
+
+            if (!isGrowing.HasValue)
+            {
+                Console.WriteLine("Функция линейна");
+            }
+            else
+            {
+                Console.WriteLine(String.Format("Функция на промежутке [-infinity; {0}) {1}," +
+                    " на промежутке ( {2}; +infinity] {3}",x0, isGrowing.Value ? down : up, x0,  isGrowing.Value ? up : down));
+            }
+        }
+    }
+}
